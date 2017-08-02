c1d9df44602bf5b65b18d2c40186fedcc5a6b943
Suren Atoyan <s.atoyan@apollobytes.com>
2017-08-02 19:02:06 +0400

    Add api for core and employees

---
+++ monitoring/routes/core/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const { execFile } = require('child_process');
+
+module.exports.post = ({ body: payload }, res) => {
+
+	const path = require('path');
+
+	const { execFile } = require('child_process');
+
+	execFile(path.join(__dirname, 'core.sh'), (err, stdout) => {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log(stdout);
+		}
+	});
+
+	res.end();
+}

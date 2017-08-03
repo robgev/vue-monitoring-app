5db133009038031ebe54d611a53d229a02b8e694
Suren Atoyan <s.atoyan@apollobytes.com>
2017-08-03 12:49:38 +0400

    Convert bitbucket payload

--- deployment/middlewares/monitoring.js
+++ deployment/middlewares/monitoring.js
@@ -1,6 +1,19 @@
+const BitbucketPayload = require('bitbucket-payload');
+
+const repositoryEvent = 'push';
+
 module.exports = ({ body: payload }, res, next) => {
 
-	console.log('Log ::: payload ::: ', payload);
+	const bitbucketPayload = new BitbucketPayload(payload, repositoryEvent);
+	const payloadObject = bitbucketPayload.getPurged();
+	const messages = [];
+
+	for (const commit of payloadObject.commits) {
+		messages.push(commit.message);
+	}
+
+	console.log('Log ::: payloadObject ::: ', payloadObject);
+	console.log('Log ::: messages ::: ', messages); 
 
 	next();
 }

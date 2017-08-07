af121935a16db828bbcfd190c7fcbbab3c5bab51
Suren Atoyan <s.atoyan@apollobytes.com>
2017-08-07 15:44:30 +0400

    Add VueJs simple client structure for QA env

---
+++ deployment/public/monitoring/src/components/Hello.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h4>{{ msgStatus }}</h4>
+    <md-progress class="md-accent" md-indeterminate></md-progress>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Monitoring App for QA environment',
+      msgStatus: 'coming soon',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- deployment/routes/home.js
+++ deployment/routes/home.js
@@ -1,5 +1,5 @@
 const path = require('path');
 
 module.exports.get = function(req, res) {
-  res.render('../views/home');
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 }

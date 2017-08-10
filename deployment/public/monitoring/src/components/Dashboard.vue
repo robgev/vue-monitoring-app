1200f764fd834814c49ab5e3be4b36291bceb64f
Robert Gevorgyan <robert1999.g@gmail.com>
2017-08-10 15:01:30 +0400

    Shape the initial layout

--- deployment/public/monitoring/src/components/Dashboard.vue
+++ deployment/public/monitoring/src/components/Dashboard.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-    <h4>{{ msgStatus }}</h4>
-    <md-button v-on:click="clickButton" class="md-raised md-primary">Test for Socket</md-button>
-    <md-progress class="md-accent" md-indeterminate></md-progress>
+    <h1 class="title">Projects</h1>
+    <hr />
+    <company-card company-name="HanseaticSoft" logo-url="static/img/hs.png" />
   </div>
 </template>
 
 <script>
+  import CompanyCard from './CompanyCard';
+
   export default {
     sockets: {
       connect() {
@@ -21,20 +22,17 @@
 
     name: 'hello',
 
-    data () {
-      return {
-        msg: 'Welcome to Monitoring App for QA environment',
-        msgStatus: 'coming soon',
-      }
-    },
-
     methods: {
       clickButton(e) {
           console.log(this.$store.state.count);
-          // $socket is socket.io-client instance 
+          // $socket is socket.io-client instance
           this.$store.commit('increment');
           this.$socket.emit('emit_method', {msg: this.$store.state.count});
       }
+    },
+
+    components: {
+      'company-card': CompanyCard
     }
   }
 </script>
@@ -52,10 +50,19 @@
 
   li {
     display: inline-block;
-    margin: 0 10px;
+    padding: 0 10px;
   }
 
   a {
     color: #42b983;
+  }
+
+  hr {
+    margin-bottom: 2rem;
+  }
+
+  .title {
+    padding-top: 1rem;
+    padding-bottom: 0.5rem;
   }
 </style>
--- deployment/public/monitoring/src/material/index.js
+++ deployment/public/monitoring/src/material/index.js
@@ -5,3 +5,17 @@ import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.css';
 
 Vue.use(VueMaterial);
+
+Vue.material.registerTheme('default', {
+  primary: {
+    color: 'cyan',
+    hue: 500,
+    textColor: 'white',
+  },
+  accent: {
+    color: 'pink',
+    hue: 'A200',
+  },
+  warn: 'red',
+  background: 'grey'
+})

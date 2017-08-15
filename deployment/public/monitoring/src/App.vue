cf366ac118baef7c4832914ec1f952874809b7f4
Robert Gevorgyan <robert1999.g@gmail.com>
2017-08-15 16:20:07 +0400

    Adjust stack animation transition

--- deployment/public/monitoring/src/App.vue
+++ deployment/public/monitoring/src/App.vue
@@ -22,16 +22,16 @@ export default {
 }
 
 .slide-fade-enter-active {
-  transition: all .3s ease;
+  transition: all .3s;
   display: none;
 }
 
 .slide-fade-leave-active {
-  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  transition: all .3s;
 }
 
 .slide-fade-enter, .slide-fade-leave-to {
-  transform: translateX(10px);
+  transform: translateX(-10px);
   opacity: 0;
 }
 

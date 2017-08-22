5edae3d274c4ba823e5312eb8f1e06939cffacdc
Robert Gevorgyan <robert1999.g@gmail.com>
2017-08-22 17:36:53 +0400

    Extract commit card as a component

---
+++ deployment/public/monitoring/src/components/CommitCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <md-card :class="`commit-card ${open ? 'active' : ''}`">
+    <md-card-header>
+      <md-card-media class="avatar">
+        <img :src="avatar" alt="People">
+      </md-card-media>
+      <md-card-header-text>
+        <div class="md-title">Commit {{message}} by {{name}}</div>
+        <div class="md-subhead">Commited on {{new Date(date).toLocaleString()}} Hash: {{hash}}</div>
+      </md-card-header-text>
+      <md-button class="md-fab md-clean" title="Checkout to this commit">
+        <md-icon>sync</md-icon>
+      </md-button>
+    </md-card-header>
+  </md-card>
+</template>
+
+<script>
+  export default {
+    props: [
+      'open',
+      'name',
+      'message',
+      'date',
+      'avatar',
+      'hash',
+    ]
+  }
+</script>
+
+<style lang="scss" scoped>
+.commit-card {
+  opacity: 0;
+  height: 0;
+  transition-property: height;
+  transition-duration: .4s;
+  transition-timing-function: cubic-bezier(.25,.8,.25,1);
+  .md-card-header {
+    align-items: center;
+    .md-title {
+      margin-top: 0 !important;
+    }
+  }
+  .avatar {
+    height: 50px;
+    img {
+      height: 50px;
+      width: auto;
+    }
+  }
+  &.active {
+    opacity: 1;
+    margin: 20px 5% 0 5%;
+    height: 100px;
+  }
+}
+</style>
--- deployment/public/monitoring/src/components/PushCard.vue
+++ deployment/public/monitoring/src/components/PushCard.vue
@@ -14,25 +14,25 @@
         <md-ink-ripple></md-ink-ripple>
       </md-card>
     </div>
-    <md-card v-for="commit in commits" :key="commit.hash" :class="`commit-card ${open ? 'active' : ''}`">
-      <md-card-header>
-        <md-card-media class="avatar">
-          <img :src="commit.author.avatar" alt="People">
-        </md-card-media>
-        <md-card-header-text>
-          <div class="md-title">Commit {{commit.message}} by {{commit.author.name}}</div>
-          <div class="md-subhead">Commited on {{new Date(commit.date).toLocaleString()}} Hash: {{commit.hash}}</div>
-        </md-card-header-text>
-        <md-button class="md-fab md-clean" title="Checkout to this commit">
-          <md-icon>sync</md-icon>
-        </md-button>
-      </md-card-header>
-    </md-card>
+    <commit-card
+      v-for="commit in commits" :key="commit.hash"
+      :open="open"
+      :name="commit.author.name"
+      :message="commit.message"
+      :date="commit.date"
+      :avatar="commit.author.avatar"
+      :hash="commit.hash"
+    />
   </div>
 </template>
 
 <script>
+  import CommitCard from './CommitCard';
+
   export default {
+    components: {
+      'commit-card': CommitCard,
+    },
     methods: {
       test() {
         this.open = !this.open;
@@ -60,32 +60,6 @@
   margin: 15px;
   .avatar {
     margin-right: 20px;
-  }
-}
-
-.commit-card {
-  opacity: 0;
-  height: 0;
-  transition-property: height;
-  transition-duration: .4s;
-  transition-timing-function: cubic-bezier(.25,.8,.25,1);
-  .md-card-header {
-    align-items: center;
-    .md-title {
-      margin-top: 0 !important;
-    }
-  }
-  .avatar {
-    height: 50px;
-    img {
-      height: 50px;
-      width: auto;
-    }
-  }
-  &.active {
-    opacity: 1;
-    margin: 20px 5% 0 5%;
-    height: 100px;
   }
 }
 </style>

743813a0f40b03cdf51044c6867d62068ac4b1eb
Suren Atoyan <s.atoyan@apollobytes.com>
2017-08-22 15:19:06 +0400

    Beautify routes-map

--- deployment/routes/index.js
+++ deployment/routes/index.js
@@ -1,41 +1,41 @@
 module.exports = function(app) {
-	// home
-	app.get(	'/',							require('./home'			).get);
+  // home
+  app.get(  '/',                            require('./home'            ).get);
 
-	// blog
-	app.post(	'/api/projects/blog', 			require('./blog/'			).post);
+  // blog
+  app.post( '/api/projects/blog',           require('./blog/'           ).post);
 
-	// maintenance
-	app.post(	'/api/projects/maintenance', 	require('./maintenance/'	).post);
+  // maintenance
+  app.post( '/api/projects/maintenance',    require('./maintenance/'    ).post);
 
-	// crewing
-	app.post(	'/api/projects/crewing', 		require('./crewing/'		).post);
+  // crewing
+  app.post( '/api/projects/crewing',        require('./crewing/'        ).post);
 
-	// miscellaneous
-	app.post(	'/api/projects/miscellaneous',	require('./miscellaneous/'	).post);
+  // miscellaneous
+  app.post( '/api/projects/miscellaneous',  require('./miscellaneous/'  ).post);
 
-	// disturbance
-	app.post(	'/api/projects/disturbance', 	require('./disturbance/'	).post);
+  // disturbance
+  app.post( '/api/projects/disturbance',    require('./disturbance/'    ).post);
 
-	// inspections
-	app.post(	'/api/projects/inspections', 	require('./inspections/'	).post);
+  // inspections
+  app.post( '/api/projects/inspections',    require('./inspections/'    ).post);
 
-	// certificates
-	app.post(	'/api/projects/certificates', 	require('./certificates/'	).post);
+  // certificates
+  app.post( '/api/projects/certificates',   require('./certificates/'   ).post);
 
-	// circulars
-	app.post(	'/api/projects/circulars', 		require('./circulars/'		).post);
+  // circulars
+  app.post( '/api/projects/circulars',      require('./circulars/'      ).post);
 
-	// towage
-	app.post(	'/api/projects/towage', 		require('./towage/'			).post);
+  // towage
+  app.post( '/api/projects/towage',         require('./towage/'         ).post);
 
-	// core
-	app.post(	'/api/projects/core', 			require('./core/'			).post);
+  // core
+  app.post( '/api/projects/core',           require('./core/'           ).post);
 
-	// employees
-	app.post(	'/api/projects/employees', 		require('./employees/'		).post);
+  // employees
+  app.post( '/api/projects/employees',      require('./employees/'      ).post);
 
-	// for all projects - sync-up
-	app.post(	'/api/projects/syncup', 		require('./syncup/'			).post);
+  // for all projects - sync-up
+  app.post( '/api/projects/syncup',         require('./syncup/'         ).post);
 
 }

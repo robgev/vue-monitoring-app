c17cf2e8f5c5faacac870cd907c09571f5299ade
Robert Gevorgyan <robert1999.g@gmail.com>
2017-08-22 17:19:44 +0400

    Add click logic to collapse commit cards when inactive

--- deployment/public/monitoring/src/components/PushCard.vue
+++ deployment/public/monitoring/src/components/PushCard.vue
@@ -1,18 +1,20 @@
 <template>
   <div class='push-card-container'>
-    <md-card md-with-hover>
-      <md-card-header>
-        <md-card-media class="avatar">
-          <img :src="avatar" alt="People">
-        </md-card-media>
-        <md-card-header-text>
-          <div class="md-title">{{name}} pushed to repo {{repo}} - {{message}}</div>
-          <div class="md-subhead">On {{new Date(date).toLocaleString()}}</div>
-        </md-card-header-text>
-      </md-card-header>
-      <md-ink-ripple></md-ink-ripple>
-    </md-card>
-    <md-card v-for="commit in commits" :key="commit.hash" class="commit-card">
+    <div @click="test">
+      <md-card md-with-hover>
+        <md-card-header>
+          <md-card-media class="avatar">
+            <img :src="avatar" alt="People">
+          </md-card-media>
+          <md-card-header-text>
+            <div class="md-title">{{name}} pushed to repo {{repo}} - {{message}}</div>
+            <div class="md-subhead">On {{new Date(date).toLocaleString()}}</div>
+          </md-card-header-text>
+        </md-card-header>
+        <md-ink-ripple></md-ink-ripple>
+      </md-card>
+    </div>
+    <md-card v-for="commit in commits" :key="commit.hash" :class="`commit-card ${open ? 'active' : ''}`">
       <md-card-header>
         <md-card-media class="avatar">
           <img :src="commit.author.avatar" alt="People">
@@ -31,6 +33,16 @@
 
 <script>
   export default {
+    methods: {
+      test() {
+        this.open = !this.open;
+      }
+    },
+    data() {
+      return {
+        open: false
+      }
+    },
     props: [
       'name',
       'repo',
@@ -44,6 +56,7 @@
 
 <style lang="scss" scoped>
 .push-card-container {
+  height: 100%;
   margin: 15px;
   .avatar {
     margin-right: 20px;
@@ -51,7 +64,11 @@
 }
 
 .commit-card {
-  margin: 20px 5% 0 5%;
+  opacity: 0;
+  height: 0;
+  transition-property: height;
+  transition-duration: .4s;
+  transition-timing-function: cubic-bezier(.25,.8,.25,1);
   .md-card-header {
     align-items: center;
     .md-title {
@@ -64,6 +81,11 @@
       height: 50px;
       width: auto;
     }
+  }
+  &.active {
+    opacity: 1;
+    margin: 20px 5% 0 5%;
+    height: 100px;
   }
 }
 </style>

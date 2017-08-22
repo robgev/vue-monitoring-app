89c2faf27ccbbe308ee7946b4c8ca9cd283225b5
Robert Gevorgyan <robert1999.g@gmail.com>
2017-08-22 14:26:03 +0400

    Fix same element displayed few times because of dummy push

--- deployment/public/monitoring/src/store/mutations.js
+++ deployment/public/monitoring/src/store/mutations.js
@@ -2,11 +2,15 @@ import * as types from './mutation-types'
 
 const mutations = {
   [types.ADD_PUSH_DATA] (state, { pushData }) {
-    const companyName = pushData.push.company
-    const projectName = pushData.push.repo
-    const currentCompany = state.companies[companyName]
-    const currentProject = currentCompany.projects[projectName]
-    currentProject.changes.push(pushData)
+    const companyName = pushData.push.company;
+    const projectName = pushData.push.repo;
+    const currentCompany = state.companies[companyName];
+    const currentProject = currentCompany.projects[projectName];
+    const { changes } = currentProject;
+    const isNew = !(changes.find(change => change.uuid === pushData.uuid)) // Search the element
+    if (isNew) {
+      currentProject.changes.push(pushData)
+    }
   },
 }
 
